933d8b0e3a0c6b0177c906b306c15f24a6e57f2b
Sascha Angermann <sascha.angermann@combridge-itc.de>
2025-03-17 16:11:50 +0100

    start switching to defining scenarios

--- scripts/test.js
+++ scripts/test.js
@@ -8,16 +8,24 @@ const duration = __ENV.DURATION || '10s';
 const amount = Number(__ENV.AMOUNT) || 10;
 
 export let options = {
-    vus,
-    duration,
+    scenarios: {
+        requests_per_minute: {
+            executor: 'constant-arrival-rate',
+            rate: 30, // 60 requests per minute (1 RPS)
+            timeUnit: '1m',
+            duration: '1m', // Run for 5 minutes
+            preAllocatedVUs: 10, // Pre-allocate 10 VUs
+            maxVUs: 20, // Allow scaling up to 20 VUs if needed
+        },
+    },
 };
 
 export default function () {
-    const payload = JSON.stringify({ amount });
+    const payload = JSON.stringify({ password: 'Ahadhsoaihsdpoahs oh' });
     const params = { headers: { 'Content-Type': 'application/json' } };
 
     const res = http.post(
-        `http://${appHost}:${port}/app/api/rest`,
+        `http://${appHost}:${port}/api/14`,
         payload,
         params
     );
